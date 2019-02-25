3bdc8b89f0fbd0d333bbbc5b27440e3f8730c2ff
xhyzzZ <41977460+xhyzzZ@users.noreply.github.com>
2019-02-25 18:10:48 -0500

    Update test_duration.py

--- test_duration.py
+++ test_duration.py
@@ -1,17 +1,10 @@
-from ffprobe import ffprobe_sync
 from pytest import approx
+import subprocess
 
 
 def test_duration():
-    fnin = 'test.mp4'
-    fnout = '480p_video1.mp4'
-    fnout2 = '720p_video1.mp4'
-
-    orig_meta = ffprobe_sync(fnin)
-    orig_duration = float(orig_meta['streams'][0]['duration'])
-
-    meta_480 = ffprobe_sync(fnout)
-    duration_480 = float(meta_480['streams'][0]['duration'])
-    meta_720 = ffprobe_sync(fnout2)
-    duration_720 = float(meta_720['streams'][0]['duration'])
-    assert orig_duration == approx(duration_480) == approx(duration_720)
+    inp='test.mp4'
+    out='480p_video1.mp4'
+    outLen = subprocess.call(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',out])
+    inLen  = subprocess.call(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',inp])
+    assert inLen == approx(outLen)
